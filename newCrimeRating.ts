c70bc4437913208a35278d5acf3055a5b88b70f4
Rahul Kumar <85476994+rahulbkumar@users.noreply.github.com>
2024-08-03 17:48:30 -0400

    Update newCrimeRating.ts

--- newCrimeRating.ts
+++ newCrimeRating.ts
@@ -1,27 +1,37 @@
-type newrimeRating = {
+type newCrimeRating = {
     rating: number;
     description: string;
   };
   
   function getCrimeRating(): CrimeRating {
-    const crimes = Math.floor(Math.random() * 7); // Random number between 0 and 6
+    //randomizes from 0-6
+    const crimes = Math.floor(Math.random() * 7);
     let rating: number;
     let description: string;
   
     switch (true) {
-      case crimes === 0:
+        // error handling
+    case crimes < 0:
+        rating = 0;
+        description = "No data available";
+        break;
+        // 0
+    case crimes === 0:
         rating = 10;
         description = "Very safe";
         break;
-      case crimes >= 1 && crimes <= 2:
+        // 1-2
+    case crimes >= 1 && crimes <= 2:
         rating = 20;
         description = "Safe";
         break;
-      case crimes >= 3 && crimes <= 5:
+        // 3-5
+    case crimes >= 3 && crimes <= 5:
         rating = 40;
         description = "Risky";
         break;
-      default:
+    default:
+        // 5+
         rating = 60;
         description = "Very risky";
     }
